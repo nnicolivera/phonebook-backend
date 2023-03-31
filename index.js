96f6c7e07265fc8a49b3721244414d3abb934ff9
Nicolás Olivera <nnicolivera@hotmail.com>
2023-03-31 15:59:12 -0300

    phonebook backend step 8 (3.8) complete

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-
-app.use(morgan('tiny'));
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json());
 
 let persons = [
