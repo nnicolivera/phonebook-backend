1c4c7c7801142fa3b4438b95bf47e049c291ea63
Nicolás Olivera <nnicolivera@hotmail.com>
2023-03-31 19:20:30 -0300

    phonebook backend connected to frontend

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 const morgan = require('morgan');
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
@@ -92,7 +94,7 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
